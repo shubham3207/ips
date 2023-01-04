95b13a994d002553b2ebb85aee8de099e30a85f2
Pitambar Koirala <pitambar.koirala@dishhome.com.np>
2023-01-04 19:50:09 +0545

    in the middle

--- app/src/main/java/org/pitambar/ins/MyTextureView.java
+++ app/src/main/java/org/pitambar/ins/MyTextureView.java
@@ -5,8 +5,12 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.PaintFlagsDrawFilter;
 import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
 import android.graphics.SurfaceTexture;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -145,9 +149,17 @@ public class MyTextureView extends TextureView implements TextureView.SurfaceTex
 //        canvas.drawBitmap(image, 0, 0, null);
         canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
 //        mImage = Bitmap.createScaledBitmap(mImage, canvas.getWidth(), canvas.getHeight(),true);
-        canvas.drawBitmap(mImage, mX, mY, mPaint);
+//        canvas.drawBitmap(mImage, mX, mY, mPaint);
 //        canvas.drawBitmap(mImage, 0, 0, mPaint);
-        canvas.drawCircle(mWidth/2, mHeight/2, 20f, mPaint);
+           Bitmap bit =  drawCircle(mImage);
+           if(mPositionArray.size()>1) {
+
+               float tempX = mPositionArray.get(mPositionArray.size() - 1)[0]-mPositionArray.get(mPositionArray.size() - 2)[0];
+               float tempY = mPositionArray.get(mPositionArray.size() - 1)[1]-mPositionArray.get(mPositionArray.size() - 2)[1];
+
+               canvas.drawBitmap(bit, tempX, tempY, mPaint);
+           }else   canvas.drawBitmap(bit, mX, mY, mPaint);
+//        canvas.drawCircle(mWidth/2, mHeight/2, 20f, mPaint);
 //        canvas.drawBitmap(mMarker, mWidth/2-(mMarker.getWidth()/2), mHeight/2- (mMarker.getHeight()/2), mPaint);
 
 //        canvas.rotate(20,mWidth/2, mHeight/2);
@@ -155,6 +167,31 @@ public class MyTextureView extends TextureView implements TextureView.SurfaceTex
         paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.FILL);
         canvas.save();
+        // Draw the circle at the current position
+//        if (mPositionArray.size() > 0) {
+//            for(int i = 0;i<mPositionArray.size(); i++){
+//                canvas.drawCircle(mPositionArray.get(i)[0], mPositionArray.get(i)[1], 10f, paint);
+//                if(mPositionArray.size()>1 && i<=mPositionArray.size()-2){
+//                    canvas.drawLine(mPositionArray.get(i)[0],mPositionArray.get(i)[1], mPositionArray.get(i+1)[0], mPositionArray.get(i+1)[1], paint);
+//                }
+//            }
+//            postInvalidate();
+//        }
+        canvas.restore();
+
+
+        mSurface.unlockCanvasAndPost(canvas);
+    }
+    private  Bitmap drawCircle(Bitmap bm) {
+
+        Bitmap bmOverlay = Bitmap.createBitmap(bm.getWidth(), bm.getHeight(), bm.getConfig());
+        Canvas canvas = new Canvas(bmOverlay);
+        Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+        canvas.drawBitmap(bm, new Matrix(), null);
+//        canvas.drawCircle(bm.getWidth()/2, bm.getHeight()/2, 50, paint);
+
+        //draw arry
         // Draw the circle at the current position
         if (mPositionArray.size() > 0) {
             for(int i = 0;i<mPositionArray.size(); i++){
@@ -165,28 +202,55 @@ public class MyTextureView extends TextureView implements TextureView.SurfaceTex
             }
             postInvalidate();
         }
-        canvas.restore();
 
 
-        mSurface.unlockCanvasAndPost(canvas);
+        return bmOverlay;
     }
+
+
+
+//    private static Bitmap drawCircle(Bitmap bitmap) {
+//        int size = Math.min(bitmap.getWidth(), bitmap.getHeight());
+//
+//        Bitmap localBitmap = Bitmap.createBitmap(size, size,
+//                Bitmap.Config.ARGB_8888);
+//        Canvas localCanvas = new Canvas(localBitmap);
+//        localCanvas.setDrawFilter(new PaintFlagsDrawFilter(0,
+//                Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG));
+//        Paint paint = new Paint();
+//        paint.setColor(Color.BLUE);
+//        paint.setAntiAlias(true);/*  www .  ja  v  a 2 s  . com*/
+//
+//        int left = bitmap.getWidth() / 2 - size / 2;
+//        int top = bitmap.getHeight() / 2 - size / 2;
+//
+//        RectF rectF = new RectF(0, 0, size, size);
+////        localCanvas.drawOval(rectF, paint);
+//        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+//        localCanvas.drawBitmap(bitmap, -left, -top, paint);
+//        localCanvas.drawOval(rectF, paint);
+//
+//        paint.setXfermode(null);
+//
+//        return localBitmap;
+//    }
     public void update() {
         // Update the x-coordinate of the image's position based on the velocity
         mX += mVelocityX;
         //update the y-coordinate of the image's position based on the velocity
         mY += mVelocityY;
 
-        // If the image has moved off the screen, wrap it around to the other side
-        if (mX > mWidth) {
-            mX = -mImage.getWidth();
-        } else if (mX + mImage.getWidth() < 0) {
-            mX = mWidth;
-        }
-
-        if (mY > mHeight) {
-            mY = -mImage.getHeight();
-        } else if (mY + mImage.getHeight() < 0) {
-            mY = mHeight;
-        }
+//        // If the image has moved off the screen, wrap it around to the other side
+//        if (mX > mWidth) {
+//            mX = -mImage.getWidth();
+//        } else if (mX + mImage.getWidth() < 0) {
+//            mX = mWidth;
+//        }
+//
+//        if (mY > mHeight) {
+//            mY = -mImage.getHeight();
+//        } else if (mY + mImage.getHeight() < 0) {
+//            mY = mHeight;
+//        }
     }
 }
